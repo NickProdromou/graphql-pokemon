f3150e1debe3033b438c9efde66e2f268ca6a80f
NickProdromou <hello@nickpro.com.au>
2018-02-12 06:05:11 +1100

    adds footer

--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -3,6 +3,7 @@ import Styled from "styled-components";
 import { Page } from "hedron";
 
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const App = () => (
   <SiteContainer fluid>
@@ -12,7 +13,7 @@ const App = () => (
         To get started, edit <code>src/App.js</code> and save to reload.
       </p>
     </Page>
-    <footer>this is the footer</footer>
+    <Footer />
   </SiteContainer>
 );
 
