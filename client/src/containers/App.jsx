5e10320c08ae2d5c25aaae77f94f4837206c7dbc
NickProdromou <hello@nickpro.com.au>
2018-02-12 05:19:32 +1100

    setup styles directory, lay foundation for layout

--- client/src/containers/App.jsx
+++ client/src/containers/App.jsx
@@ -1,14 +1,29 @@
 import React from "react";
+import Styled from "styled-components";
+import { Page } from "hedron";
 
 const App = () => (
-  <div className="App">
+  <SiteContainer fluid className="App">
     <header className="App-header">
       <h1 className="App-title">Welcome to React</h1>
     </header>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
-  </div>
+    <MainSection fluid>
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    </MainSection>
+    <footer>this is the footer</footer>
+  </SiteContainer>
 );
+
+const SiteContainer = Styled.div`
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+`;
+
+const MainSection = Styled(Page)`
+  flex: 1;
+`;
 
 export default App;
